8e3723b201193651eea9442fdeaf01c06950558d
Sam Mullins <sammullins.co.uk@gmail.com>
2026-03-12 20:02:14 +0000

    Better homepage

--- apps/homepage/src/apps-data.ts
+++ apps/homepage/src/apps-data.ts
@@ -4,6 +4,7 @@ export interface AppInfo {
   category: string;
   description: string;
   url: string;
+  image?: string;
 }
 
 export const appsData: AppInfo[] = [
@@ -13,6 +14,7 @@ export const appsData: AppInfo[] = [
     category: "Letters and Numbers",
     description: "A simple educational game for toddlers where they identify and type numbers on a moving London bus.",
     url: "/apps/letters-and-numbers/NumberBus/index.html",
+    image: "./src/assets/number-bus.png"
   },
   {
     id: "bingo-buddy",
@@ -20,6 +22,7 @@ export const appsData: AppInfo[] = [
     category: "Letters and Numbers",
     description: "A colorful and interactive Bingo caller app for children with animations and voice announcements.",
     url: "/apps/letters-and-numbers/bingo-buddy/index.html",
+    image: "./src/assets/bingo-buddy.png"
   },
   {
     id: "number-plate-pop",
@@ -27,5 +30,8 @@ export const appsData: AppInfo[] = [
     category: "Letters and Numbers",
     description: "A fun and simple number plate matching game designed for toddlers.",
     url: "/apps/letters-and-numbers/number-plate-pop/index.html",
+    image: "./src/assets/number-plate-pop.png"
   }
+
 ];
+
